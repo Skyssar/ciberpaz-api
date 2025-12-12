1b62f978004e00e2fb8b0be7e7db0defc04c2844
Skyssar <ycure10@gmail.com>
2025-12-12 00:19:22 -0500

    migrations on posgres

---
+++ ciberpaz_api/Migrations/20251212051052_PostgresMigration.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ciberpaz_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostgresMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "menuitem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Main = table.Column<bool>(type: "boolean", nullable: false),
+                    Icon = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_menuitem", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "views",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_views", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "paragraphs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    ViewId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_paragraphs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_paragraphs_views_ViewId",
+                        column: x => x.ViewId,
+                        principalTable: "views",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sections",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: true),
+                    Link = table.Column<string>(type: "text", nullable: true),
+                    ViewId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sections", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_sections_views_ViewId",
+                        column: x => x.ViewId,
+                        principalTable: "views",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_paragraphs_ViewId",
+                table: "paragraphs",
+                column: "ViewId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_sections_ViewId",
+                table: "sections",
+                column: "ViewId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "menuitem");
+
+            migrationBuilder.DropTable(
+                name: "paragraphs");
+
+            migrationBuilder.DropTable(
+                name: "sections");
+
+            migrationBuilder.DropTable(
+                name: "views");
+        }
+    }
+}
